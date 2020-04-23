5974e1c6f7eaff6b7fc43d3fd987909d8c65a991
marioR <mariojosue155@gmail.com>
2020-04-23 17:18:01 -0600

    Add: Labo 05 Ejercicio 03

---
+++ Labo05/Ejercicio03/Labo05_ Ej_03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Labo05__Ej_03
+{
+    class Program
+    {
+
+        static void Recursividad(int num, int n, Boolean check) {
+            if (check) {
+                if (num < n)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                    Recursividad(num, n, true);
+                }
+                else if (num == n)
+                {
+                    Console.Write(num + " ");
+                    num--;
+                    Recursividad(num, n, false);
+                }
+            }else {
+                    if (num > 1)
+                    {
+                        Console.Write(num + " ");
+                        num--;
+                        Recursividad(num, n, check);
+                    }else if (num == 1)
+                    {
+                        Console.Write(num);
+                    }
+                }
+        }
+        
+        
+        public static void Main(string[] args) {
+            Console.Write("digitar numero para recorrido. ");
+
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            if (n > 0) {
+              Recursividad(num: 1, n: n, check: true);
+            }else     
+                Console.Write("el numero no se puede ejecutar.");
+
+        }
+    }
+}
