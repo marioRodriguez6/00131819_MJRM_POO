a6b4b4131a4f92ef4cd09aae2202d74256d1eeb6
marioR <mariojosue155@gmail.com>
2020-04-23 14:57:21 -0600

    Add: Labo 05 Ej. 1

---
+++ Labo05/Ejercicio01/Labo_Ej01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Labo_Ej01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca el valor de n donde terminaremos: ");
+            int lim = 0;
+            lim = Convert.ToInt32(Console.ReadLine());
+
+            int n1 = 0;
+            int n2 = 1;
+            var reserv =0;
+
+            if (lim == 1 ) {
+                Console.Write("0. ");
+            }
+            else{
+                Console.Write("0. 1. ");
+                for (var i = 1; (i) < lim - 1; (i)++)
+                {
+                    reserv = n1;
+                    n1 = n2;
+
+                    n2 = n1 + reserv;
+                    Console.Write(n2 + ". ");
+                }
+            }
+        }
+    }
+}
