ab7bb1899a6903431052a31b87ee1d60a2698aed
marioR <mariojosue155@gmail.com>
2020-04-23 16:28:34 -0600

    Add: Labo 05 Ejercicio 02

---
+++ Labo05/Ejecicio02/Labo 05_Ej_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Labo_05_Ej_02 {
+    class Program{
+
+        public static String Palindromo(String word)
+        {
+            char[] letter = word.ToCharArray();
+            
+            Array.Reverse(letter);
+            return new string(letter);
+        }
+
+         static void Main(String[] args) {
+            
+            String word = "ara";
+
+            if (word.Equals(Palindromo(word))) {
+                Console.Write("es palindromo");
+                
+            }
+            else
+            {
+                Console.Write("no es palindromo");
+            }
+
+        }
+    }
+}
