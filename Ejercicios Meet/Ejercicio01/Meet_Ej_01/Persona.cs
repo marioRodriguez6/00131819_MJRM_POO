dc0d72762fc5521262028544d7c33a300a8dc0e2
marioR <mariojosue155@gmail.com>
2020-04-23 21:57:32 -0600

    Add: Primer Ejercicio Meet

---
+++ Ejercicios Meet/Ejercicio01/Meet_Ej_01/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Meet_Ej_01
+{
+    public class Persona
+    {
+        private String nombre, apellido;
+        private int edad;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public Persona(string nombre = "sin nombre", string apellido = "no apellido", int edad = 0) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(nombre)}: {nombre}, {nameof(apellido)}: {apellido}, {nameof(edad)}: {edad}";
+        }
+        
+        public String Saludar(String saludoP)
+        {
+            return nombre + " " + saludoP;
+        }
+        
+    }
+}
---
+++ Ejercicios Meet/Ejercicio01/Meet_Ej_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meet_Ej_01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new List<Persona>();
+
+            p.Add(new Persona("mario", "Rodriguez", 19));
+            p.Add(new Persona("Luis", edad: 20));
+            p.Add(new Persona(edad: 22));
+            p.Add(new Persona(apellido: "Morales"));
+
+
+            String buscar = "walter";
+            buscar = Console.ReadLine();
+            
+            p.ForEach(it => {
+                if (buscar != null && buscar.Equals(it.Nombre))
+                {
+                    Console.WriteLine("Nuevo nombre: ");
+                    it.Nombre = Console.ReadLine();
+                    Console.WriteLine(it.ToString());
+                }
+            });
+
+            int añadir = 12;
+            int arbol = 14;
+        }
+    }
+}
