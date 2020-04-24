a4e40a0b1a5464a5c784083827b8097298049509
marioR <mariojosue155@gmail.com>
2020-04-23 18:15:30 -0600

    Add: Labo 05 Ejercicio 04

---
+++ Labo05/Ejercicio04/Labo05_Ej04/Program.cs
@@ -0,0 +1,18 @@
+﻿  using System;
+
+namespace Labo05_Ej04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digiste la frase que quiere cambiar.");
+            String phrase = "";
+            phrase = Console.ReadLine();
+
+            if (phrase != null) phrase = phrase.Replace("a", "b");
+            
+            Console.Write(phrase);
+        }
+    }
+}
