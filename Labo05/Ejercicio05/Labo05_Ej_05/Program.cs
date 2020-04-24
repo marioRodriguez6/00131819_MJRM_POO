e3828727c51a468701a402ea6534ce384497bf16
marioR <mariojosue155@gmail.com>
2020-04-23 23:37:36 -0600

    Modified: Labo05 Ejercicio 05

--- Labo05/Ejercicio05/Labo05_Ej_05/Program.cs
+++ Labo05/Ejercicio05/Labo05_Ej_05/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Labo05_Ej_05
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<char> vowels = new List<char>();
+
+            Boolean checkvalid = true;
+
+            while (checkvalid)
+            {
+                Console.Write("ingrese una letra: ");
+                char letter = ' ';
+                letter = Convert.ToChar(Console.ReadLine());
+
+                switch (letter)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                        vowels.Add(letter);
+                        break;
+                }
+
+                Console.Write("Desea terminar ?  (s/n)");
+                char check = ' ';
+                check = Convert.ToChar(Console.ReadLine());
+                checkvalid = check == 'n';
+            }
+
+            foreach (var vocals in vowels)
+                Console.Write(vocals + " ");
         }
     }
 }
